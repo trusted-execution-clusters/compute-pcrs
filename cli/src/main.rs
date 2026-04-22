f49dc4d30ee34b37dd05747a82c44a6624edeeb5
Beñat Gartzia Arruabarrena <bgartzia@redhat.com>
2026-04-22 12:48:10 +0200

    cli: compute PCR4 for uki images
    Now users can pass uki and uki addons paths to the compute-pcrs binary.
    Now, the binary will assume that it is predicting PCRs for an UKI image.
    It will try to find the UKI image in the path provided by the user. If
    it was not provided, it will try finding it in the default path. It it
    was not found even there, then it will assume it is the legacy case and
    will compute pcr4 as it was done until now.
    
    Signed-off-by: Beñat Gartzia Arruabarrena <bgartzia@redhat.com>

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -10,8 +10,6 @@ use clap::{Args, Parser, Subcommand};
 use log::LevelFilter;
 use serde::{Deserialize, Serialize};
 
-use std::path::PathBuf;
-
 use compute_pcrs_lib::*;
 
 #[derive(Parser, Debug)]
@@ -51,10 +49,16 @@ enum Command {
         secureboot_variables: SecureBootVarStores,
         #[arg(
             long,
-            default_value_t = false,
-            help = "Indicates that the linux image is an UKI image (e.g. is not vmlinuz))"
+            short,
+            default_value = "",
+            help = "Path to the UKI binary. It will try to find it in ${rootfs}/boot/EFI/Linux/*.efi by default."
         )]
-        uki: bool,
+        uki: String,
+        #[arg(
+            long,
+            help = "Path to a UKI addon relative to the root dir. It can be passed multiple times."
+        )]
+        uki_addon: Vec<String>,
         #[arg(
             long = "secureboot-disabled",
             default_value_t = false,
@@ -68,29 +72,12 @@ enum Command {
         )]
         mok_variables: String,
     },
-    /// Compute PCR 4 for the non UKI case
-    Pcr4NoUki {
-        #[arg(
-            long,
-            short,
-            default_value = "/",
-            help = "Path to the target container image root filesystem"
-        )]
-        rootfs: String,
-        #[arg(
-            long,
-            default_value_t = false,
-            help = "Indicates that the linux image is an UKI image (e.g. is not vmlinuz))"
-        )]
-        uki: bool,
-        #[arg(
-            long = "secureboot-disabled",
-            default_value_t = false,
-            help = "Compute PCRs as if secure boot was disabled in the system"
-        )]
-        no_secureboot: bool,
-    },
-    /// Compute PCR 4 for the Bootable Container UKI case
+    /// Compute PCR 4
+    ///
+    /// It will try to find the UKI in the user-provided path. If empty,
+    /// it will assume the default Bootable Container UKI path:
+    /// ${rootfs}/boot/EFI/Linux/*.efi.
+    /// If not found, it will then assume that it is the non UKI case.
     Pcr4 {
         #[arg(
             long,
@@ -102,12 +89,21 @@ enum Command {
         #[arg(
             long,
             short,
-            default_value = "/boot/EFI/Linux/uki.efi",
-            help = "Path to the UKI binary"
+            default_value = "",
+            help = "Path to the UKI binary. It will try to find it in ${rootfs}/boot/EFI/Linux/*.efi by default."
         )]
         uki: String,
-        #[arg(long, help = "Path to a UKI addon (can be passed multiple times)")]
+        #[arg(
+            long,
+            help = "Path to a UKI addon relative to the root dir. It can be passed multiple times."
+        )]
         uki_addon: Vec<String>,
+        #[arg(
+            long = "secureboot-disabled",
+            default_value_t = false,
+            help = "Compute PCRs as if secure boot was disabled in the system"
+        )]
+        no_secureboot: bool,
     },
     /// Compute PCR 7
     Pcr7 {
@@ -168,12 +164,19 @@ fn main() -> Result<()> {
             rootfs,
             secureboot_variables,
             uki,
+            uki_addon,
             no_secureboot,
             mok_variables,
         } => {
-            let rfs = rootfs::RootFSTree::new(rootfs).unwrap();
+            let rfs = rootfs::RootFSTree::new(rootfs, uki, uki_addon.clone()).unwrap();
             let pcrs = vec![
-                compute_pcr4_nouki(rfs.vmlinuz(), rfs.esp(), *uki, !no_secureboot),
+                compute_pcr4(
+                    rfs.vmlinuz(),
+                    rfs.esp(),
+                    rfs.uki(),
+                    rfs.uki_addons(),
+                    !no_secureboot,
+                ),
                 compute_pcr7(
                     secureboot_variables.efivars.as_deref(),
                     rfs.esp(),
@@ -188,23 +191,22 @@ fn main() -> Result<()> {
             );
             Ok(())
         }
-        Command::Pcr4NoUki {
-            rootfs,
-            uki,
-            no_secureboot,
-        } => {
-            let rfs = rootfs::RootFSTree::new(rootfs).unwrap();
-            let pcr = compute_pcr4_no_uki(rfs.vmlinuz(), rfs.esp(), *uki, !no_secureboot);
-            println!("{}", serde_json::to_string_pretty(&pcr).unwrap());
-            Ok(())
-        }
         Command::Pcr4 {
             rootfs,
             uki,
             uki_addon,
+            no_secureboot,
         } => {
             log::debug!("{uki_addon:?}");
-            let pcr = compute_pcr4(rfs.esp(), uki, uki_addon);
+            let rfs = rootfs::RootFSTree::new(rootfs, uki, uki_addon.clone()).unwrap();
+            let pcr = compute_pcr4(
+                rfs.vmlinuz(),
+                rfs.esp(),
+                rfs.uki(),
+                rfs.uki_addons(),
+                !no_secureboot,
+            );
+
             println!("{}", serde_json::to_string_pretty(&pcr).unwrap());
             Ok(())
         }
@@ -213,7 +215,7 @@ fn main() -> Result<()> {
             secureboot_variables,
             no_secureboot,
         } => {
-            let rfs = rootfs::RootFSTree::new(rootfs).unwrap();
+            let rfs = rootfs::RootFSTree::new(rootfs, "", vec![]).unwrap();
             let pcr = compute_pcr7(
                 secureboot_variables.efivars.as_deref(),
                 rfs.esp(),
